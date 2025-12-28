71aa7bfc026479533acd3ec65f383f74fb6991c9
Inna Khomovska <innapariy@gmail.com>
2025-12-28 13:28:54 +0100

    fix: add useLocation to MovieList for correct back navigation state

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,9 +1,10 @@
-
+import { useLocation } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import styles from './MovieList.module.css';
 
 const MovieList = ({ movies, showImage = true }) => {
- 
+const location = useLocation();
+
   const getMovieTitleWithYear = (title, releaseDate) => {
     const year = releaseDate ? new Date(releaseDate).getFullYear() : ''; 
     return year ? `${title} (${year})` : title; 
@@ -13,7 +14,7 @@ const MovieList = ({ movies, showImage = true }) => {
     <ul className={styles.movieList}>
       {movies.map((movie) => (
         <li key={movie.id} className={styles.movieItem}>
-          <Link to={`/movies/${movie.id}`} state={{ from: location.pathname }}>
+          <Link to={`/movies/${movie.id}`} state={{ from: location }}>
             <h2>{getMovieTitleWithYear(movie.title, movie.release_date)}</h2> 
             {showImage && movie.poster_path && (
               <img className={styles.moviePoster}
